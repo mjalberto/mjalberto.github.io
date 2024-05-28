9350042e06155cd3b4547fd0f8668e117041d686
Markimoo <mjalberto1998@gmail.com>
2024-05-28 19:12:45 +0800

    font adjusment

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-function menu(){
-    var element = document.getElementById("menu");
-    element.classList.toggle('active');
+function menu() {
+  var element = document.getElementById("menu");
+  element.classList.toggle("active");
 }
