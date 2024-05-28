28c07167b1b373f7aa5fb68e5faa0a75667f47bd
Markimoo <mjalberto1998@gmail.com>
2024-05-28 21:26:29 +0800

    hmm

--- js/script.js
+++ js/script.js
@@ -3,9 +3,11 @@ function menu() {
   element.classList.toggle("active");
 }
 
+classList.contains("active");
+
 ScrollReveal({
   reset: true,
-  distance: "2rem",
+  distance: "5%",
   duration: "1000",
   // delay: "200",
   // origin: "left",
@@ -14,20 +16,20 @@ ScrollReveal({
 // HOME
 
 ScrollReveal().reveal(".img01", { origin: "right", delay: "200" });
-ScrollReveal().reveal(".sub01", { origin: "left", delay: "100" });
+ScrollReveal().reveal(".sub01", { origin: "left", delay: "300" });
 ScrollReveal().reveal(".title", { origin: "right", delay: "300" });
 ScrollReveal().reveal(".description", { origin: "bottom", delay: "600" });
 
 // ABOUT
 
 ScrollReveal().reveal(".img02", { origin: "left", delay: "200" });
-ScrollReveal().reveal(".sub02", { origin: "top", delay: "100" });
-ScrollReveal().reveal(".s01-p01", { origin: "right", delay: "300" });
-ScrollReveal().reveal(".s01-p02", { origin: "left", delay: "600" });
+ScrollReveal().reveal(".sub02", { origin: "right", delay: "300" });
+ScrollReveal().reveal(".s01-p01", { origin: "left", delay: "300" });
+ScrollReveal().reveal(".s01-p02", { origin: "right", delay: "600" });
 ScrollReveal().reveal(".s01-p03", { origin: "bottom", delay: "900" });
 
 // CONTACT
 
-ScrollReveal().reveal(".img03", { origin: "left", delay: "200" });
-ScrollReveal().reveal(".sub03", { origin: "top", delay: "100" });
-ScrollReveal().reveal(".s03-p01", { origin: "right", delay: "300" });
+ScrollReveal().reveal(".img03", { origin: "left", delay: "100" });
+ScrollReveal().reveal(".sub03", { origin: "right", delay: "300" });
+ScrollReveal().reveal(".s03-p01", { origin: "bottom", delay: "600" });
