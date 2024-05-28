0738a0ff76b8890a4a0bef58ee3b3ab6ebcaa3ac
Markimoo <mjalberto1998@gmail.com>
2024-05-28 20:31:37 +0800

    scroll reveal added

--- js/script.js
+++ js/script.js
@@ -2,3 +2,32 @@ function menu() {
   var element = document.getElementById("menu");
   element.classList.toggle("active");
 }
+
+ScrollReveal({
+  reset: true,
+  distance: "2rem",
+  duration: "1000",
+  // delay: "200",
+  // origin: "left",
+});
+
+// HOME
+
+ScrollReveal().reveal(".img01", { origin: "right", delay: "200" });
+ScrollReveal().reveal(".sub01", { origin: "left", delay: "100" });
+ScrollReveal().reveal(".title", { origin: "right", delay: "300" });
+ScrollReveal().reveal(".description", { origin: "bottom", delay: "600" });
+
+// ABOUT
+
+ScrollReveal().reveal(".img02", { origin: "left", delay: "200" });
+ScrollReveal().reveal(".sub02", { origin: "top", delay: "100" });
+ScrollReveal().reveal(".s01-p01", { origin: "right", delay: "300" });
+ScrollReveal().reveal(".s01-p02", { origin: "left", delay: "600" });
+ScrollReveal().reveal(".s01-p03", { origin: "bottom", delay: "900" });
+
+// CONTACT
+
+ScrollReveal().reveal(".img03", { origin: "left", delay: "200" });
+ScrollReveal().reveal(".sub03", { origin: "top", delay: "100" });
+ScrollReveal().reveal(".s03-p01", { origin: "right", delay: "300" });
